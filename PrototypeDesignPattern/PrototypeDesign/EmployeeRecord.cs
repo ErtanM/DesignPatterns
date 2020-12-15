839e3039738178d43a98e1f9504eb9e6fa5cb2d2
Ertan Mutlu <33805792+ErtanM@users.noreply.github.com>
2020-12-15 20:06:47 +0200

    Add files via upload

---
+++ PrototypeDesignPattern/PrototypeDesign/EmployeeRecord.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypeDesign
+{
+    public class EmployeeRecord : IPrototype
+    {
+        private readonly int id;
+        private readonly string name, designation;
+        private readonly double salary;
+        private readonly string address;
+
+        public EmployeeRecord()
+        {
+            Console.WriteLine("    Employee Records of Oracle Corporation");
+            Console.WriteLine("-".PadRight(10));
+            Console.WriteLine($"{id} {name} {designation} {salary} {address}");
+        }
+
+        public EmployeeRecord(int id, string name, string designation, string address, double salary) : base()
+        {
+            this.id = id;
+            this.name = name;
+            this.address = address;
+            this.designation = designation;
+            this.salary = salary;
+        }
+
+        public void ShowRecord()
+        {
+            string pad = "-";
+            Console.WriteLine("Employee records of Oracle Corp.");
+            Console.WriteLine(pad.PadRight(10));
+            Console.WriteLine($"Eid {name} {designation} {salary} {address} \n");
+        }
+
+        public IPrototype GetClone()
+        {
+            return new EmployeeRecord(id, name, designation, address, salary);
+        }
+
+    }
+}
---
+++ PrototypeDesignPattern/PrototypeDesign/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypeDesign
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter employee Id: ");
+            int.TryParse(Console.ReadLine().Trim(), out int eId);
+
+            Console.Write("Enter employee Name: ");
+            string eName = Console.ReadLine().Trim();
+
+            Console.Write("Enter employee Designation: ");
+            string eDesignation = Console.ReadLine().Trim();
+
+            Console.Write("Enter employee Address: ");
+            string eAddress = Console.ReadLine().Trim();
+
+            Console.Write("Enter employee Salary: ");
+            double.TryParse(Console.ReadLine().Trim(), out double eSalary);
+
+            Console.WriteLine();
+
+            EmployeeRecord e1 = new EmployeeRecord(eId, eName, eDesignation, eAddress, eSalary);
+
+            e1.ShowRecord();
+
+            EmployeeRecord e2 = (EmployeeRecord)e1.GetClone();
+
+            e2.ShowRecord();
+
+            Console.ReadLine();
+        }
+    }
+}
